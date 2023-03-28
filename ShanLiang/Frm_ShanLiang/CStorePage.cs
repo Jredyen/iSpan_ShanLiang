296299af691eeb811b17023af389d179cc784d31
ChihHsun96 <a9627864900@gmail.com>
2023-03-28 17:43:56 +0800

    StorePage

--- ShanLiang/Frm_ShanLiang/CStorePage.cs
+++ ShanLiang/Frm_ShanLiang/CStorePage.cs
@@ -15,23 +15,24 @@ namespace Frm_ShanLiang
         List<Store> _list = new List<Store>(); //建立Store類別的集合
         public event CStoreShow cSS;//委派方法
         public static int _sid = 1; //店家的ID
-        public CStorePage()
-        {
-            LoadStoreData();            
-        }  
-
-        public void LoadStoreData()
-        {
-            Store store = new Store();//新Store物件
-            var qName = SLE.Stores.Where(id => id.StoreID == _sid).Select(n => n.RestaurantName);
-            foreach (var item in qName)
-                store.RestaurantName = item;
-            _list.Add(store);
-        }
-        public Store getCurrent()
-        {
-            return _list[_sid];
-        }
+        
+        
+        //public CStorePage()
+        //{
+        //    LoadStoreData();            
+        //}  
+        //public void LoadStoreData()
+        //{
+        //    Store store = new Store();//新Store物件
+        //    var qName = SLE.Stores.Where(id => id.StoreID == _sid).Select(n => n.RestaurantName);
+        //    foreach (var item in qName)
+        //        store.RestaurantName = item;
+        //    _list.Add(store);
+        //}
+        //public Store getCurrent()
+        //{
+        //    return _list[_sid];
+        //}
         public void OutsideShow()
         {            
             cSS();
