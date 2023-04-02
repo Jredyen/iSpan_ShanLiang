ae2fc5cb5a9b100570e4bf2578eb1d2a16ba7e51
棨惟 謝 <kevin19861109@hotmail.com>
2023-04-02 22:37:11 +0800

    解決傳統的Windows Forms在高解析度（High DPI）設定下，所引發的文字模糊問題

--- ShanLiang/Frm_ShanLiang/Program.cs
+++ ShanLiang/Frm_ShanLiang/Program.cs
@@ -9,12 +9,16 @@ namespace ShanLiang
 {
     internal static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
         /// <summary>
         /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
         static void Main()
         {
+            //解決傳統的Windows Forms在高解析度（High DPI）設定下，所引發的文字模糊問題
+            if (System.Environment.OSVersion.Version.Major >= 6) { SetProcessDPIAware(); }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Frm_Homepage());
