0be6b014fd7c0f1f4a7b8fbe030e166ee16080b4
Yu-Kuei <kingsley110011@gmail.com>
2023-04-04 20:56:43 +0800

    會員頁面 1.資料修改完成 2.訂單紀錄部分功能先不做 3.優惠券頁面先不做 4.會員行動只先做查詢功能

--- ShanLiang/Frm_ShanLiang/Frm_MemberPage.cs
+++ ShanLiang/Frm_ShanLiang/Frm_MemberPage.cs
@@ -23,14 +23,15 @@ namespace Frm_ShanLiang
     }
     private void Frm_MemberPage_Load(object sender, EventArgs e)
     {
-        txt_MemberSearchByWords.SetWatermark("請輸入餐廳名稱");
-    }
+        //txt_MemberSearchByWords.SetWatermark("請輸入餐廳名稱");
+       
+        }
         //private void btn_ReviseData_Click(object sender, EventArgs e)
         //{
            
         //}
         ShanLiangEntities _SL = new ShanLiangEntities();
-        
+        BindingSource BindingSourceMemberOrderDetails = new BindingSource();
            
     private void btn_MemberRevisingInfo_Click(object sender, EventArgs e)
         {
@@ -67,6 +68,17 @@ namespace Frm_ShanLiang
             foreach (var i in qAddress){
                 this.txt_MemberAddress.Text = i;
             }
+            this.txt_MemberPasswords.Enabled = false;
+            this.txt_MemberPasswordsConfirmation.Enabled = false;
+            this.rdo_encryption.Enabled = false;
+            this.rdoDecryption.Enabled = false; this.label_NewPasswords.Enabled = false;
+            this.label_MemberNewPasswordsConfirm.Enabled = false;
+            this.txt_MemberPasswords.Visible = false;
+            this.txt_MemberPasswordsConfirmation.Visible = false;
+            this.rdo_encryption.Visible = false;
+            this.rdoDecryption.Visible = false;
+            this.label_NewPasswords.Visible = false;
+            this.label_MemberNewPasswordsConfirm.Visible = false;
         }
     private void btn_MemberOrderRecord_Click(object sender, EventArgs e)
     {
@@ -167,7 +179,7 @@ namespace Frm_ShanLiang
            
             _SL.SaveChanges();
         }
-
+        bool IsPasswordsEnabled = false;
         private void btn_MemberInfoRevisionComfirm_Click(object sender, EventArgs e)
         {
             try
@@ -177,13 +189,13 @@ namespace Frm_ShanLiang
                          where m.AccountName == CNowLoginAccount.loginAccountName
                          select m).FirstOrDefault();
                 if (q == null) return;
-                if (boolStg == false)
+                if (boolStg == false && IsPasswordsEnabled == true)
                 {
                     
                     MessageBox.Show("密碼不符格式");
                     return;
                 }
-                if (IsConsistency == false)
+                if (IsConsistency == false && IsPasswordsEnabled == true)
                 {
                     MessageBox.Show("密碼不一致");
                     return;
@@ -211,52 +223,68 @@ namespace Frm_ShanLiang
 
         private void btn_MemberInfoLoad_Click(object sender, EventArgs e)
         {
-             //從db載入會員資料
-            var qAccount = from p in _SL.Members
-                       where p.AccountName == CNowLoginAccount.loginAccountName
-                       select p.AccountName;
-            foreach (var i in qAccount) {
-                this.txt_MemberAccount.Text = i;
+            try
+            {
+                //從db載入會員資料
+                var qAccount = from p in _SL.Members
+                               where p.AccountName == CNowLoginAccount.loginAccountName
+                               select p.AccountName;
+                foreach (var i in qAccount)
+                {
+                    this.txt_MemberAccount.Text = i;
+                }
+                var qName = from p in _SL.Members
+                            where p.AccountName == CNowLoginAccount.loginAccountName
+                            select p.MemberName;
+                foreach (var i in qName)
+                {
+                    this.txt_MemberName.Text = i;
+                }
+                var qPhone = from p in _SL.Members
+                             where p.AccountName == CNowLoginAccount.loginAccountName
+                             select p.Memberphone;
+                foreach (var i in qPhone)
+                {
+                    this.txt_MemberTelephoneNumber.Text = i;
+                }
+                var qEmail = from p in _SL.Members
+                             where p.AccountName == CNowLoginAccount.loginAccountName
+                             select p.Email;
+                foreach (var i in qEmail)
+                {
+                    this.txt_MemberEMail.Text = i;
+                }
+                var qAddress = from p in _SL.Members
+                               where p.AccountName == CNowLoginAccount.loginAccountName
+                               select p.Address;
+                foreach (var i in qAddress)
+                {
+                    this.txt_MemberAddress.Text = i;
+                }
             }
-            var qName = from p in _SL.Members
-                        where p.AccountName == CNowLoginAccount.loginAccountName
-                        select p.MemberName;
-            foreach(var i in qName){
-                this.txt_MemberName.Text = i;
-            }
-            var qPhone = from p in _SL.Members
-                         where p.AccountName == CNowLoginAccount.loginAccountName
-                         select p.Memberphone;
-            foreach (var i in qPhone){
-                this.txt_MemberTelephoneNumber.Text = i;
-            }    
-            var qEmail = from p in _SL.Members
-                         where p.AccountName == CNowLoginAccount.loginAccountName
-                         select p.Email;
-            foreach (var i in qEmail){
-                this.txt_MemberEMail.Text = i;
-            }
-            var qAddress = from p in _SL.Members
-                       where p.AccountName == CNowLoginAccount.loginAccountName
-                       select p.Address;
-            foreach (var i in qAddress){
-                this.txt_MemberAddress.Text = i;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
             }
         }
 
         private void btn_MemberInfoRevisionDemo_Click(object sender, EventArgs e)
         {
+            try { 
             //demo 修改會員電話與地址
-            this.txt_MemberPasswords.Text = "ispanR304";
             this.txt_MemberTelephoneNumber.Text = "0983385150";
             this.txt_MemberAddress.Text = "新北市土城區";
-            this.txt_MemberPasswordsConfirmation.Text = "ispanR304";
+                }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
+            }
 
         }
 
         private void btn_ClearMemberInfo_Click(object sender, EventArgs e)
         {
-            
+            try { 
                 this.txt_MemberAccount.Text = "";
                 this.txt_MemberPasswords.Text = "";
                 this.txt_MemberPasswordsConfirmation.Text = "";
@@ -264,6 +292,11 @@ namespace Frm_ShanLiang
                 this.txt_MemberTelephoneNumber.Text = "";
                 this.txt_MemberEMail.Text = "";
                 this.txt_MemberAddress.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
+            }
         }
 
         private void rdo_encryption_CheckedChanged(object sender, EventArgs e)
@@ -288,10 +321,115 @@ namespace Frm_ShanLiang
 
         private void btn_MemberSearch_Click(object sender, EventArgs e)
         {
-            var q = from m in this._SL.Members
-                    where m.AccountName == CNowLoginAccount.loginAccountName
-                    select m.Orders;
-            this.dataGridView1.DataSource = q.ToList();
+            try
+            {
+                var q = from m in this._SL.Members
+                        join o in this._SL.Orders
+                        on m.MemberID equals o.MemberID
+                        join s in this._SL.Stores
+                        on o.StoreID equals s.StoreID
+                        where m.AccountName == CNowLoginAccount.loginAccountName
+                        //&& s.AccountName.Contains(ss)
+                        select new
+                        {
+                            /*訂單編號 =*/
+                            o.OrderID,
+                            /*會員姓名 =*/
+                            m.MemberName,
+                            /*餐廳名稱 = */
+                            s.AccountName,
+                            /*餐廳電話 =*/
+                            s.RestaurantPhone,
+                            /*餐廳地址 = */
+                            s.RestaurantAddress,
+                            /*訂位時間 = */
+                            o.ReservationTime,
+                            /*訂位人數 = */
+                            o.ReservationNumber,
+                            /*用餐金額 = */
+                            o.Amount,
+                        };
+                //var q = from m in this._SL.Members
+                //        join o in this._SL.Orders
+                //        on m.MemberID equals o.MemberID
+                //        join s in this._SL.Stores
+                //        on o.StoreID equals s.StoreID
+                //        select new
+                //        {
+                //            o.OrderID,
+                //            m.MemberName,
+                //            s.AccountName,
+                //            s.RestaurantPhone,
+                //            s.RestaurantAddress,
+                //            o.ReservationTime,
+                //            o.ReservationNumber,
+                //        };
+                //var q = from m in this._SL.Members
+                //        from o1 in m.Orders
+                //        from o2 in o1.Store.RestaurantName
+
+                //        where o1.MemberID == 33
+                //        select new
+                //        {
+                //            o1.OrderID,
+                //            o1.StoreID,
+                //            m.MemberName,
+                //            o2
+                //        };
+                //var q = from m in this._SL.Members
+                //        from o1 in m.Orders
+                //        from s in this._SL.Stores
+                //        from o2 in s.Orders
+
+
+
+                //        where o1.MemberID == 33
+                //        select new
+                //        {
+                //            o1.OrderID,o1.StoreID,m.MemberName,
+                //            o2.Store.RestaurantName
+                //        };
+                //var q =
+                //       from s in this._SL.Stores
+                //       from o in s.Orders
+
+                //       where o.MemberID == 33
+                //       select new
+                //       {
+                //           o.Store.RestaurantName,
+                //           o.Store.RestaurantAddress,
+                //           o.Store.RestaurantPhone,
+                //           s.AccountName
+                //       };
+                //var q2 = from m in this._SL.Orders
+                //        where m.MemberID == 33
+                //        select m;
+                //foreach (var i in q2) {
+
+
+                //}
+
+                BindingSourceMemberOrderDetails.DataSource = q.ToList();
+                this.dataGridView1.DataSource = BindingSourceMemberOrderDetails;
+
+                //MessageBox.Show(BindingSourceMemberOrderDetails[1].ToString());
+                //this.listBox1.Items.Add(BindingSourceMemberOrderDetails);
+
+                this.label_MemberOrderId.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "OrderID");
+                this.label_RestaurantName.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "AccountName");
+                this.label_RestaurantPhonenumber.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "RestaurantPhone");
+                this.label_RestaurantAddress.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "RestaurantAddress");
+                this.label_MemberReservationTime.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "ReservationTime");
+                this.label_MemberReservationNumber.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "ReservationNumber");
+                this.label_MemberSpend.DataBindings.Add("Text", BindingSourceMemberOrderDetails, "Amount");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外");
+            }
+
+
+
 
         }
         bool IsConsistency;
@@ -314,6 +452,134 @@ namespace Frm_ShanLiang
                 this.label_MemberPassworsConsistency.Text = "";
                 IsConsistency = true;
             }
+        }
+
+        private void btn_MemberEnalblePassword_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.txt_MemberPasswords.Enabled = true;
+                this.txt_MemberPasswordsConfirmation.Enabled = true;
+                this.rdo_encryption.Enabled = true;
+                this.rdoDecryption.Enabled = true;
+                this.label_MemberNewPasswordsConfirm.Enabled = true;
+                this.txt_MemberPasswords.Visible = true;
+                this.label_NewPasswords.Enabled = true;
+                this.txt_MemberPasswords.Visible = true;
+                this.txt_MemberPasswordsConfirmation.Visible = true;
+                this.rdo_encryption.Visible = true;
+                this.rdoDecryption.Visible = true;
+                this.label_NewPasswords.Visible = true;
+                this.label_MemberNewPasswordsConfirm.Visible = true;
+                IsPasswordsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
+            }
+        }
+
+        private void tabPage1_MemberAction_Click(object sender, EventArgs e)
+        {
+           }
+        
+
+        private void btn_MemberActionChaningedConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbo_MemberSelectedAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try { 
+            
+            var q = from a in _SL.Actions
+                    join ma in _SL.Member_Action
+                    on a.ActionID equals ma.ActionID
+                    join s in _SL.Stores
+                    on ma.StoreID equals s.StoreID
+                    //where AccountAndAction((Int32)ma.MemberID, (Int32)ma.ActionID )
+
+                    where ma.MemberID == 33
+                    &&
+                    ma.ActionID == cbo_MemberSelectedAction.SelectedIndex+1
+                    select new {a.ActionName, s.AccountName };
+            this.dataGridView2.DataSource = q.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
+            }
+            //foreach (var item in q)
+            //{
+            //    item.ActionName= "收藏";
+            //}
+        }
+        bool AccountAndAction(int ii1, int ii2)
+        {
+            bool result1 = ii1 == CNowLoginAccount.nowLoginAccountID;
+            bool result2 = ii2 == cbo_MemberSelectedAction.SelectedIndex;
+            return result1 && result2;
+        }
+
+        private void btn_MemberInfoRevisionDemoWithPasswords_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //demo 修改會員電話與地址
+                this.txt_MemberPasswords.Text = "ispanR304";
+                this.txt_MemberTelephoneNumber.Text = "0983385150";
+                this.txt_MemberAddress.Text = "新北市土城區";
+                this.txt_MemberPasswordsConfirmation.Text = "ispanR304";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外事件");
+            }
+
+        }
+
+        private void btn_MemberInfoRevisionComfirmWithPasswords_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //修改會員資料
+                var q = (from m in this._SL.Members
+                         join a in this._SL.Accounts
+                         on m.AccountName equals a.AccountName
+                         where m.AccountName == CNowLoginAccount.loginAccountName
+                         select m ).FirstOrDefault();
+                if (q == null) return;
+                if (boolStg == false && IsPasswordsEnabled == true)
+                {
+
+                    MessageBox.Show("密碼不符格式");
+                    return;
+                }
+                if (IsConsistency == false && IsPasswordsEnabled == true)
+                {
+                    MessageBox.Show("密碼不一致");
+                    return;
+                }
+                //修改會員姓名
+                q.MemberName = this.txt_MemberName.Text;
+                //修改會員電話
+                q.Memberphone = this.txt_MemberTelephoneNumber.Text;
+                //修改會員姓名
+                q.Email = this.txt_MemberEMail.Text;
+                //修改會員地址
+                q.Address = this.txt_MemberAddress.Text;
+                //存入db
+                this._SL.SaveChanges();
+                MessageBox.Show("修改成功");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生{ex}例外");
+
+            }
+
         }
     }
 }
